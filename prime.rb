4f43af02bae1cacd39aaf7fef5934724b7a036ed
mkdoh3 <mkdoherty3@gmail.com>
2019-05-15 19:16:40 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+## prime numbers -> divisible only by 1 and itself
+require 'pry'
+
+def prime?(num)
+    if(num < 2)
+        return false
+    end
+    # create a range between 2 and num/2 
+    (2..num/2).none?{|i| 
+        num % i == 0
+    }
+end
+
+puts prime?(2)
